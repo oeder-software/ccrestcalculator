a252fdd4a776bf7a3eed6c8ec32f8201320611f1
oeder-software <166632558+oeder-software@users.noreply.github.com>
2024-09-07 14:46:19 +0200

    Update headerscript.js

--- headerscript.js
+++ headerscript.js
@@ -1,4 +1,4 @@
 if (parent.document.title == "home") {
   document.querySelector("body").style =
-    "background-image:  url(/img/logo.png), url(/img/oederlogo.png),url(/img/headerbg.png);";
+    "background-image:  url(./img/logo.png), url(./img/oederlogo.png)";
 }
